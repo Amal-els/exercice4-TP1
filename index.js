e12825a02796ee06231cf98ae82f10e27c81d8ea
Amal-els <amalbahri19@gmail.com>
2025-02-17 15:55:19 +0100

    latest commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+function gettingRandomColor(){
+    return (`rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`)
+}
 var item = document.querySelectorAll("li");
 item.forEach(function(item){
     item.addEventListener("click",function(){
-    item.style.color = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`;
-})})
+    item.style.color = gettingRandomColor();})
+    }
+)
