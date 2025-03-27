0e1786fa7a392c1581302f0c23dc1b3a6f29ca23
Amal-els <amalbahri19@gmail.com>
2025-03-27 11:31:40 +0100

    last commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function gettingRandomColor(){
     return (`rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`);
-}let item = document.querySelectorAll("li");
+}
+let item = document.querySelectorAll("li");
 item.forEach(function(item){
     item.addEventListener("click",function(){
     item.style.color = gettingRandomColor();})
